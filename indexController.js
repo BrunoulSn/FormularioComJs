e0f5b44c58f726c80a3441d11f2f647b7658fd64
Bruno Cristiano <bruno120505@gmail.com>
2025-04-03 21:45:05 -0300

    Arrumado

--- indexController.js
+++ indexController.js
@@ -1,37 +1,26 @@
+
 function CadastroUsuario(){
-    let nome = document.getElementById("NomeUsuario");
-    let email = document.getElementById("EmailUsuario");
-    let senha = document.getElementById("senha");
+  let nome = document.getElementById("NomeUsuario");
+  let email = document.getElementById("EmailUsuario");
+  let senha = document.getElementById("SenhaUsuario");
+  let senhaCriptografada = CryptoJS.AES.encrypt(senha.value, 'chaveSecreta').toString();
 
     let cadastro = {
-        nome: nome,
-        email: email,
-        senha: senha
-      };
-      
-      // Importar a biblioteca crypto-js
-const CryptoJS = require('crypto-js');
-
-
-// Criptografar a senha
-let senhaCriptografada = CryptoJS.AES.encrypt(cadastro.senha, 'chaveSecreta').toString();
-
-// Substituir a senha original pela senha criptografada
-cadastro.senha = senhaCriptografada;
-
+        nome: nome.value,
+        email: email.value,
+        senha: senhaCriptografada.value,
+      };      
 // Converter o objeto para uma string JSON e armazenar no LocalStorage
 localStorage.setItem('cadastro', JSON.stringify(cadastro));
 
 // Recuperar os dados do LocalStorage e converter de volta para um objeto JSON
 let cadastroRecuperado = JSON.parse(localStorage.getItem('cadastro'));
 
-// Descriptografar a senha
+/*// Descriptografar a senha
 let senhaDescriptografada = CryptoJS.AES.decrypt(cadastroRecuperado.senha, 'chaveSecreta').toString(CryptoJS.enc.Utf8);
 
 // Substituir a senha criptografada pela senha descriptografada
 cadastroRecuperado.senha = senhaDescriptografada;
-
+*/
 console.log(cadastroRecuperado);
-
-      
-}
+    }
